5ab52327d4901e36a134396d5b526915676b960b
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2024-01-05 14:39:13 +0530

    fix bug

--- navigation/AuthNav.tsx
+++ navigation/AuthNav.tsx
@@ -29,11 +29,11 @@ const AuthNav = () => {
         headerLeft: ({ canGoBack }) => canGoBack && <HeaderBackBtn />,
       }}
     >
-      <AuthStack.Screen name="Gender" component={Gender} />
       <AuthStack.Screen name="Welcome" component={Welcome} />
       <AuthStack.Screen name="Login" component={Login} />
       <AuthStack.Screen name="ProfilePicture" component={ProfilePicture} />
       <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <AuthStack.Screen name="Gender" component={Gender} />
       <AuthStack.Screen name="Email" component={Email} />
       <AuthStack.Screen name="Birthday" component={Birthday} />
       <AuthStack.Screen name="Bio" component={Bio} />
